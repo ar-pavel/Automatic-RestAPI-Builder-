bf4b87e96bc1e11bfc8b0a03bbdc2f9c543c7d11
ar-pavel <atikur1998@yahoo.com>
2020-10-19 13:39:02 +0600

    Building from JSON implemented

--- src/index.js
+++ src/index.js
@@ -32,10 +32,99 @@ endpointBuilder.build(baseUrl, "os", ["DELETE"]);
 // for testing perpose manual call has been made
 // endpointBuilder.build(baseUrl, "student");
 // endpointBuilder.build(baseUrl, "employee");
-endpointBuilder.build(baseUrl, "intiser", ["GET"]);
-endpointBuilder.build(baseUrl, "sojal", ["GET", "PUT"]);
-endpointBuilder.build(baseUrl, "pavel", ["GET", "POST", "DELETE"]);
+// endpointBuilder.build(baseUrl, "intiser", ["GET"]);
+// endpointBuilder.build(baseUrl, "sojal", ["GET", "PUT"]);
+// endpointBuilder.build(baseUrl, "pavel", ["GET", "POST", "DELETE"]);
 
-endpointBuilder.build(baseUrl, "fahim", ["GET", "POST", "PUT", "DELETE"]);
+// endpointBuilder.build(baseUrl, "fahim", ["GET", "POST", "PUT", "DELETE"]);
+
+const request = 
+  {
+    "model" :
+      [
+        {
+          "modelName": "Student",
+          "modelAttributes": [
+            {
+              "name": "studentID",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            },
+            {
+              "name": "studentName",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            }
+          ],
+          "apiEndPoints": ["GET", "POST"]
+        },
+        {
+          "modelName": "Teacher",
+          "modelAttributes": [
+            {
+              "name": "teacherInitial",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            },
+            {
+              "name": "teacherName",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            }
+          ],
+          "apiEndPoints": ["GET", "POST", "DELETE", "PUT"]
+        },
+        {
+          "modelName": "Employee",
+          "modelAttributes": [
+            {
+              "name": "employeeID",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            },
+            {
+              "name": "employeeName",
+              "type": "String",
+              "required": true,
+              "default": "N/A"
+            }
+          ],
+          "apiEndPoints": ["GET", "POST"]
+        }
+      ],
+    
+    "database" : {     
+        "applicable": true,
+        "dbProperty": {
+          "dbName": "MongoDB",
+          "misc": "N/A"
+      }
+    },
+    
+    // {
+    //   "type": "",
+    //   "attributes": {}
+    // },
+  };
+
+  // console.log(typeof(request.model));
+  // console.log(request);
+
+  console.log(request.model.map(x => x))
+
+
+
+  request.model.map(x =>  endpointBuilder.build(baseUrl, x.modelName, x.apiEndPoints)  )
+
 
 // --------------------------------------------------------------------
+
+/* TODO:
+ *       - add MySQL Server
+ *       - configure API calls
+ */
